a196dc8be47ff5d45acee331f573ee5c6bed691e
golisn <yoonsb2@naver.com>
2022-08-09 17:43:19 +0900

    카테 및 태그 페이지

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,40 @@
 from curses.ascii import US
+from unicodedata import category
 from django.db import models
 from django.contrib.auth.models import User
+import os
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}/'
+    
+
+    class Meta():
+        verbose_name_plural = 'Categories'
+
+    
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/tag/{self.slug}/'
+
+
+
 class Post(models.Model):
     title = models.CharField(max_length=20)
+    hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()
 
     create_at = models.DateTimeField(auto_now_add=True)
@@ -13,9 +43,23 @@ class Post(models.Model):
     head_image = models.ImageField(upload_to='blog/imges/%Y/%m/%d/', blank = True)
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank = True)
 
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # author = models.ForeignKey(User, on_delete=models.CASCADE) 작성자를 지우면 포스트 지우는 코딩
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)  # 작성자를 지워도 포스트는 남는 코딩
+
+    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL, blank=True)
+
+    tags = models.ManyToManyField(Tag, blank=True)
 
     def __str__(self):
         return f'({self.pk}){self.title} :: {self.author}'
+
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]
+    
+
