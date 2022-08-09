69854a6bd7aac4a513d130fe01856b029f12aafc
golisn <yoonsb2@naver.com>
2022-08-09 12:19:45 +0900

    author 추가

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,6 @@
+from curses.ascii import US
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
@@ -8,7 +10,12 @@ class Post(models.Model):
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
+    head_image = models.ImageField(upload_to='blog/imges/%Y/%m/%d/', blank = True)
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank = True)
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
     def __str__(self):
-        return f'({self.pk}){self.title}'
+        return f'({self.pk}){self.title} :: {self.author}'
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
